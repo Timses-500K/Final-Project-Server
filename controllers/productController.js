68f61ea46a04c13facb6de9034ecbb1ddc6f220e
Zoolfan <zulfankhatami32@gmail.com>
2023-05-03 13:18:25 +0700

    Product API

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,59 @@
-const { Item } = require("../models");
-
+const { Item, Category, Size } = require("../models");
 
 class ProductController {
-    
+	static findAllItem = async (req, res, next) => {
+		try {
+			const data = await Item.findAll({
+				where: {
+					visibility: "True",
+				},
+				include: [
+					{
+						model: Category,
+						as: "itemCategory",
+						through: { attributes: [] },
+						attributes: ["categoryName"],
+					},
+					{
+						model: Size,
+						as: "itemSize",
+						through: { attributes: [] },
+						attributes: ["size"],
+					},
+				],
+			});
+			res.status(200).json(data);
+		} catch (err) {
+			next(err);
+		}
+	};
+
+	static findAllByCat = async (req, res, next) => {
+		try {
+			const { categoryName } = req.params;
+			const data = await Category.findOne({
+				where: {
+					categoryName,
+				},
+				include: [
+					{
+						model: Item,
+						as: "categoryItem",
+						through: { attributes: [] },
+						attributes: ["itemName", "price", "imageUrl"],
+					},
+				],
+			});
+
+			if (data) {
+				res.status(200).json(data);
+			} else {
+				throw { name: "ErrorNotFound" };
+			}
+		} catch (err) {
+			next(err);
+		}
+	};
 }
 
 module.exports = ProductController;
