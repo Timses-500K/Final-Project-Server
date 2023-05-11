8aac9508766002c7571626f513e214a4f1d19e67
azharitn02 <azharitn02@gmail.com>
2023-05-11 22:13:42 +0700

    Updated Middlewares Error Handler

--- middlewares/errorhandlers.js
+++ middlewares/errorhandlers.js
@@ -1,9 +1,39 @@
 const errorHandler = (err, req, res, next) => {
   console.log(err);
   if (err.name === "ErrorNotFound") {
-    console.log("MASOOK ERROR HANDLER");
+    console.log("Error Not Found");
     res.status(404).json({
       message: "Error Not Found",
+    });
+  } else if (err.name === "CategoryNotFound") {
+    console.log("Category Not Found");
+    res.status(404).json({
+      message: "Category Not Found",
+    });
+  } else if (err.name === "ItemNotFound") {
+    console.log("Item Not Found");
+    res.status(404).json({
+      message: "Item Not Found",
+    });
+  } else if (err.name === "SizeNotFound") {
+    console.log("Size Not Found");
+    res.status(404).json({
+      message: "Size Not Found",
+    });
+  } else if (err.name === "UserNotFound") {
+    console.log("User Not Found");
+    res.status(404).json({
+      message: "User Not Found",
+    });
+  } else if (err.name === "WrongEmail") {
+    console.log("Wrong Email");
+    res.status(404).json({
+      message: "Wrong Email",
+    });
+  } else if (err.name === "WrongPassword") {
+    console.log("Wrong Password");
+    res.status(404).json({
+      message: "Wrong Password",
     });
   } else {
     res.status(500).json({
