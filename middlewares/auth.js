7901c4f158760111697e960df6ecfe19c167576c
azharitn02 <azharitn02@gmail.com>
2023-05-13 23:58:48 +0700

    Updated Middleware Auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,28 +2,31 @@ require("dotenv").config();
 const jwt = require('jsonwebtoken');
 const { User } = require("../models");
 
-const auth = async (req, res, next) => {
-  try {
-    // Get the token from the request header
-    const token = req.header('Authorization').replace('Bearer ', '');
+class Auth {
+  
+  static async authentication(req, res, next){
+    try {
+      // Get the token from the request header
+      const token = req.header('Authorization').replace('Bearer ', '');
 
-    // Verify the token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // Verify the token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    // Find the user in the database
-    const user = await User.findOne({ where: { id: decoded.id } });
+      // Find the user in the database
+      const user = await User.findOne({ where: { id: decoded.id } });
 
-    // Check if the user exists
-    if (!user) {
-      throw new Error();
+      // Check if the user exists
+      if (!user) {
+        next({name: "UserNotFound"});
+      }
+
+      // Attach the user object to the request
+      req.user = user;
+      next();  
+    } catch (err) {
+      res.status(401).send({ error: 'Please Authenticate' });
     }
-
-    // Attach the user object to the request
-    req.user = user;
-    next();
-  } catch (err) {
-    res.status(401).send({ error: 'Please Authenticate' });
   }
-};
+}
 
-module.exports = { auth };
+module.exports = { Auth };
