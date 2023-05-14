adb95f381dbd2184d3b459c96702072e0d167def
azharitn02 <azharitn02@gmail.com>
2023-05-14 22:50:37 +0700

    Updated Index Route

--- routes/index.js
+++ routes/index.js
@@ -10,27 +10,19 @@ const itemRouter = require("./item"),
 	orderRouter = require("./order"),
 	productRouter = require("./product"),
 	productDetailRouter = require("./productDetail"),
-	userRouter = require("./user");
-const itemRouter = require("./item"),
-      addressRouter = require ('./address'),
-      adminRouter = require ('./admin'),
-      cartRouter = require ('./cart'),
-      dashboardRouter = require ('./dashboard'),
-      homeRouter = require ('./home'),
-      orderRouter = require ('./order'),
-      productRouter = require ('./product'),
-      productDetailRouter = require ('./productDetail'),
-      userRouter = require ('./user');
+	userRouter = require("./user"),
+	loginRegisterRouter = require("./loginRegister");
 
 router.use("/api/item", itemRouter);
-// router.use("/api/address", addressRouter);
+router.use("/api/address", addressRouter);
 // router.use("/api/admin", adminRouter);
 // router.use("/api/cart", cartRouter);
-// router.use("/api/dashboard", dashboardRouter);
-// router.use("/api/home", homeRouter);
-// router.use("/api/order", orderRouter);
+router.use("/api/dashboard", dashboardRouter);
+router.use("/api/home", homeRouter);
+router.use("/api/order", orderRouter);
 router.use("/api/product", productRouter);
 router.use("/api/productDetail", productDetailRouter);
-// router.use("/api/user", userRouter);
+router.use("/api/user", userRouter);
+router.use('/api', loginRegisterRouter)
 
 module.exports = router;
