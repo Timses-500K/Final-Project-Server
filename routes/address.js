fb210904d6ac07d88e6df8a0870a6849721d75a7
azharitn02 <azharitn02@gmail.com>
2023-05-20 06:07:35 +0700

    Updated Address route

--- routes/address.js
+++ routes/address.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const { Auth } = require('../middlewares/auth');
 const AddressController = require("../controllers/addressController.js");
 
 // Get a specific address by ID
-router.get("/:userId/:addressId", AddressController.getAddressById);
+// router.get("/:userId/:addressId", Auth.authentication, AddressController.getAddressById);
+
+// Find addresses by logged User
+router.get("/", Auth.authentication, AddressController.findAddressloggedUser);
 
 // Create a new address
-router.post("/:userId", AddressController.createAddress);
+router.post("/", Auth.authentication, AddressController.createAddress);
 
 // Update an address
-router.put("/:userId/:addressId", AddressController.updateAddress);
+router.put("/:addressId", Auth.authentication , AddressController.updateAddress);
+
+// Delete Address
+router.delete("/:addressId", Auth.authentication, AddressController.deleteAddress);
 
 module.exports = router;
