6cfecb670d8e57e7ce6ead26d6c67fa4d76be06a
WilliamFirman <william.firman25@gmail.com>
2023-05-21 01:12:19 +0700

    Add POST and DELETE for Order

--- routes/order.js
+++ routes/order.js
@@ -5,4 +5,10 @@ const orderController = require("../controllers/orderController");
 // Get order by id
 router.get("/:orderId", orderController.getOrderById);
 
+// Create order by userId
+router.post("/:userId", orderController.createOrder);
+
+// Delete order by id
+router.delete("/:orderId", orderController.deleteOrder);
+
 module.exports = router;
