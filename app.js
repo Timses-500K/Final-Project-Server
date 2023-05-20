8b736e083e352d026dd4c9351e1cd5fecbc89cbf
azharitn02 <azharitn02@gmail.com>
2023-05-20 20:59:33 +0700

    add cors

--- app.js
+++ app.js
@@ -3,18 +3,16 @@ const app = express();
 const port = 3001;
 const errorHandler = require("./middlewares/errorhandlers");
 const router = require("./routes");
+const cors = require("cors")
 
 app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
-
+app.use(cors);
 
 app.use(router);
 app.use(errorHandler);
-
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
