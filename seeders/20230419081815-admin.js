6213340b2a912209905e4e6caee933e077a578bf
azharitn02 <azharitn02@gmail.com>
2023-05-22 23:41:32 +0700

    updated admin seeding

--- seeders/20230419081815-admin.js
+++ seeders/20230419081815-admin.js
@@ -1,14 +1,16 @@
 "use strict";
+const bcrypt = require("bcrypt");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    const password = await bcrypt.hash("Admin", 10);
     await queryInterface.bulkInsert(
       "Admins",
       [
         {
-          username: "Nando",
-          password: "Nando",
+          username: "Admin",
+          password: password,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
