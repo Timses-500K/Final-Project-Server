513e4c756d1f681360bbd44ca9e467698982cef7
azharitn02 <azharitn02@gmail.com>
2023-05-23 18:07:56 +0700

    Fixed Product Detail Controller

--- controllers/productDetailController.js
+++ controllers/productDetailController.js
@@ -1,15 +1,30 @@
-const { Item, Category, Size } = require("../models");
+const { Item, Category, Size, ItemSize } = require("../models");
+const sequelize = require("sequelize")
 
 class ProductDetailController {
 	static findById = async (req, res, next) => {
 		const { id } = req.params;
 
 		try {
+			const stock = await ItemSize.findAll({
+				where: {
+				  itemId: id,				  
+				},
+			  });
+		  
+			const countStock = stock.length;
 			const data = await Item.findOne({
 				where: {
 					id,
 				},
+				attributes: {
+					include: [
+						[sequelize.literal(countStock), "countStock"]
+					  ],
+					exclude: ["stock"],
+				  },
 				include: [
+
 					{
 						model: Category,
 						as: "itemCategory",
@@ -20,9 +35,14 @@ class ProductDetailController {
 						model: Size,
 						as: "itemSize",
 						through: { attributes: [] },
-						attributes: ["size"],
+						distinct: true,
+						attributes: ["size",
+						[sequelize.fn("COUNT", sequelize.col("itemSize.id")), "itemSizeCount"],
+						],
+						
 					},
 				],
+				group: ["Item.id", "itemCategory.id", "itemSize.id"], 
 			});
 
 			if (data) {
