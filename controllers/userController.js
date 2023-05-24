c582939aabf177a329e71364ee0d148ce13fb8eb
azharitn02 <azharitn02@gmail.com>
2023-05-24 20:16:13 +0700

    Update Edit User

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,13 +15,13 @@ class UserController {
             model: Order,
             attributes: ["id", "subtotal", "totalPrice", "status"],
           },
-          {
-            model: Cart,
-            attributes: ["id", "totalPrice"],
-          },
+          // {
+          //   model: Cart,
+          //   attributes: ["id", "totalPrice","deletedAt"],
+          // },
         ],
         attributes: {
-          exclude: ["visibility"],
+          exclude: ["visibility","password"],
         },
       });
 
@@ -43,9 +43,9 @@ class UserController {
       const user = await User.findByPk(id);
       if (user) {
         await user.update({
-          username: username || user.username,
-          email: email || user.email,
-          password: password || user.password,
+          // username: username || user.username,
+          // email: email || user.email,
+          // password: password || user.password,
           firstName: firstName || user.firstName,
           lastName: lastName || user.lastName,
           birth: birth || user.birth,
@@ -60,40 +60,20 @@ class UserController {
   }
 
   // Delete user by id
-  static async delete(req, res, next) {
-    const id = req.user.id;
-    try {
-      const user = await User.findByPk(id);
-      if (user) {
-        await user.destroy();
-        res.status(200).json({ message: "User deleted successfully", user });
-      } else {
-        next({ name: "UserNotFound" });
-      }
-    } catch (err) {
-      next(err);
-    }
-  }
-
-  //find logged user
-  static async findUser(req, res, next){
-    try {
-      const userId = req.user.id;
-      const user = await User.findByPk( userId, {
-        attributes: {
-          exclude: ["visibility"],
-        },
-      });
-
-      if(!user){
-        next({name: "UserNotFound"});
-      }
-      res.status(200).json(user);
-    } catch (err) {
-      next(err);
-    }
-  };
-
+  // static async delete(req, res, next) {
+  //   const id = req.user.id;
+  //   try {
+  //     const user = await User.findByPk(id);
+  //     if (user) {
+  //       await user.destroy();
+  //       res.status(200).json({ message: "User deleted successfully", user });
+  //     } else {
+  //       next({ name: "UserNotFound" });
+  //     }
+  //   } catch (err) {
+  //     next(err);
+  //   }
+  // }
 }
 
 module.exports = UserController;
