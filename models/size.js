35bce6ffe18aa869ba5082953e7c7a2fb5d19e93
azharitn02 <azharitn02@gmail.com>
2023-05-24 17:02:16 +0700

    Add associate Size and ItemSize

--- models/size.js
+++ models/size.js
@@ -5,6 +5,7 @@ module.exports = (sequelize, DataTypes) => {
 
     static associate(models) {
       Size.belongsToMany(models.Item,{foreignKey: "sizeId", through: models.ItemSize, as: "sizeItem"});
+      Size.hasMany(models.ItemSize,{foreignKey:"sizeId"});
       Size.hasMany(models.CartItem,{foreignKey: "sizeId"});
       Size.belongsToMany(models.Cart,{foreignKey: "sizeId", through: models.CartItem, as: "sizeCart"});
       // Size.belongsToMany(models.Item,{foreignKey: "sizeId", through: models.CartItem, as: "sizeCart"});
