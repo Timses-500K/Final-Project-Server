1063dd45fd088828fa0039bccc28152f001d57b1
azharitn02 <azharitn02@gmail.com>
2023-05-25 09:30:15 +0700

    Add Associate OrderItem To Size

--- models/orderitem.js
+++ models/orderitem.js
@@ -6,13 +6,15 @@ module.exports = (sequelize, DataTypes) => {
   class OrderItem extends Model {
 
     static associate(models) {
-      OrderItem.belongsTo(models.Item,{foreignKey: "itemId"})
-      OrderItem.belongsTo(models.Order,{foreignKey: "orderId"})
+      OrderItem.belongsTo(models.Item,{foreignKey: "itemId"});
+      OrderItem.belongsTo(models.Order,{foreignKey: "orderId"});
+      OrderItem.belongsTo(models.Size,{foreignKey: "sizeId"});
     }
   }
   OrderItem.init({
     orderId: DataTypes.INTEGER,
     itemId: DataTypes.INTEGER,
+    sizeId: DataTypes.INTEGER,
     quantity: DataTypes.INTEGER
   }, {
     sequelize,
