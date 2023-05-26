9fce3aa030c8fe6339c568aacf7973698e28d7c7
azharitn02 <azharitn02@gmail.com>
2023-05-26 22:35:52 +0700

    Unfinished Cart Route

--- routes/cart.js
+++ routes/cart.js
@@ -2,12 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { Auth } = require("../middlewares/auth");
 const CartController = require("../controllers/cartController.js");
-const { Auth } = require("../middlewares/auth.js")
 
 //middleware
 router.use( Auth.author);
 
-router.use(Auth.author);
 router.get("/get/", CartController.showAllCartByUserId);
 router.post("/add/", CartController.addCart);
 router.put("/update/:cartItemId", CartController.updateCart);
