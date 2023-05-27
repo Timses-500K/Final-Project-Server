d4e5d497ff4000e3902b4a3b1502b2216cc8ed90
WilliamFirman <william.firman25@gmail.com>
2023-05-28 02:23:01 +0700

    first commit

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,456 +1,388 @@
-const { Cart, Item, CartItem, ItemSize, Size, Address } = require("../models");
+const { Cart, Item, CartItem, ItemSize, Size } = require("../models");
 
 class CartController {
-	static showAllCartByUserId = async (req, res, next) => {
-		try {
-			const userId = req.user.id;
-			const cart = await Cart.findOne({
-				where: {
-					userId:userId,
-					
-				},
-				attributes: ["id", "userId", "totalPrice"],
-			});
-			if (cart[0] == null) {
-				throw { name: "ErrorNotFound" };
-				// throw { name: "CartNotFound" };
-			}
+  static showAllCartByUserId = async (req, res, next) => {
+    try {
+      const userId = req.user.id;
+      const cart = await Cart.findOne({
+        where: {
+          userId,
+          visibility: "True",
+        },
+        attributes: ["id", "userId", "totalPrice"],
+      });
+      if (!cart) {
+        throw { name: "CartNotFound" };
+      }
 
-			const cartItem = await CartItem.findAll({
-				where: {
-					cartId: cart.id,
-				},
-				attributes: ["itemId", "quantity", "cartId"],
-				order: [["cartId", "ASC"]],
-				include: [
-					{
-						model: Item,
-						attributes: ["itemName", "price", "imageUrl"],
-						// include: [
-						// 	{
-						// 		model: Size,
-						// 		as: "itemSize",
-						// 		through: { attributes: [] },
-						// 		attributes: ["id", "size"],
-						// 	},
-						// ],
-					},
-					{
-						model: Size,
-						attributes: ["size"],
-					},
-					// {
-					// 	model: Cart,
-					// 	attributes: ["totalPrice"],
-					// },
-				],
-			});
+      const cartItem = await CartItem.findAll({
+        where: {
+          cartId: cart.id,
+        },
+        attributes: ["itemId", "quantity", "cartId"],
+        order: [["cartId", "ASC"]],
+        include: [
+          {
+            model: Item,
+            attributes: ["itemName", "price", "imageUrl"],
+            // include: [
+            // 	{
+            // 		model: Size,
+            // 		as: "itemSize",
+            // 		through: { attributes: [] },
+            // 		attributes: ["id", "size"],
+            // 	},
+            // ],
+          },
+          {
+            model: Size,
+            attributes: ["size"],
+          },
+          // {
+          // 	model: Cart,
+          // 	attributes: ["totalPrice"],
+          // },
+        ],
+      });
 
-			res.status(200).json({ cartItem, cart });
-			// res.status(200).json(cartItem);
-		} catch (err) {
-			next(err);
-		}
-	};
+      res.status(200).json({ cartItem, cart });
+      // res.status(200).json(cartItem);
+    } catch (err) {
+      next(err);
+    }
+  };
 
-	static addCart = async (req, res, next) => {
-		try {
-			const autoFillVisibility = "True";
-			const autoFillUpdatedAt = new Date();
-			const autoFillCreateddAt = new Date();
-			const userId = req.user.id;
-			const { addressId, itemId, quantity, sizeId } = req.body;
+  static addCart = async (req, res, next) => {
+    try {
+      const autoFillVisibility = "True";
+      const autoFillUpdatedAt = new Date();
+      const autoFillCreateddAt = new Date();
+      const userId = req.user.id;
+      const { addressId, itemId, quantity, sizeId } = req.body;
 
-			const item = await Item.findOne({
-				where: {
-					id: itemId,
-				},
-			});
-			if (!item) {
-				throw { name: "ItemNotFound" };
-			}
-			const price = item.price;
-			const totalPrice = price * quantity;
+      const item = await Item.findOne({
+        where: {
+          id: itemId,
+        },
+      });
+      if (!item) {
+        throw { name: "ItemNotFound" };
+      }
+      const price = item.price;
+      const totalPrice = price * quantity;
 
-			const userCart = await Cart.findOne({
-				where: {
-					userId,
-					visibility: "True",
-				},
-			});
-			if (!cart) {
-				throw { name: "ErrorNotFound" };
-				// throw { name: "CartNotFound" };
-			}
+      const userCart = await Cart.findOne({
+        where: {
+          userId,
+          visibility: "True",
+        },
+      });
+      if (!userCart) {
+        // Make a cart and cart item if the user doesn't have any active cart
+        const newCart = await Cart.create({
+          userId,
+          addressId,
+          totalPrice,
+          visibility: autoFillVisibility,
+          createdAt: autoFillCreateddAt,
+          updatedAt: autoFillUpdatedAt,
+        });
 
-			// check if the item has the size
-			const cekSize = await ItemSize.findOne({
-				where: {
-					itemId,
-					sizeId,
-				},
-			});
-			if (!cekSize) {
-				throw { name: "SizeNotFound" };
-			}
+        const newCartItem = await CartItem.create({
+          cartId: newCart.id,
+          itemId,
+          quantity,
+          sizeId,
+          createdAt: autoFillCreateddAt,
+          updatedAt: autoFillUpdatedAt,
+        });
 
-			// find all cart item which belongs to the user
-			const cartItem = await CartItem.findAll({
-				where: {
-					cartId: userCart.id,
-				},
-			});
+        res.status(201).json({
+          newCart,
+          newCartItem,
+          message: "Cart created successfully",
+        });
+      }
 
-			// extracts the property from cartItem and adds it to the new array
-			const itemIdCartItem = cartItem.map((cartItem) => cartItem.itemId);
-			const sizeIdCartItem = cartItem.map((cartItem) => cartItem.sizeId);
+      // check if the item has the size
+      const cekSize = await ItemSize.findOne({
+        where: {
+          itemId,
+          sizeId,
+        },
+      });
+      if (!cekSize) {
+        throw { name: "SizeNotFound" };
+      }
 
-			// This code will check if the user already has the same item and size in the cart
-			// if true it'll just update the item quantity,if not then the code will create a new cart item
-			if (itemIdCartItem.includes(itemId) && sizeIdCartItem.includes(sizeId)) {
-				const cartItem = await CartItem.findOne({
-					where: {
-						cartId: userCart.id,
-						itemId,
-					},
-				});
+      // find all cart item which belongs to the user
+      const cartItem = await CartItem.findAll({
+        where: {
+          cartId: userCart.id,
+        },
+      });
 
-				const priceDifference = price * cartItem.quantity - totalPrice;
+      // extracts the property from cartItem and adds it to the new array
+      const itemIdCartItem = cartItem.map(cartItem => cartItem.itemId);
+      const sizeIdCartItem = cartItem.map(cartItem => cartItem.sizeId);
 
-				const putCart = await Cart.update(
-					{
-						totalPrice: userCart.totalPrice - priceDifference,
-						updatedAt: autoFillUpdatedAt,
-					},
-					{
-						where: {
-							id: cartItem.cartId,
-						},
-					}
-				);
+      // This code will check if the user already has the same item and size in the cart
+      // if true it'll just update the item quantity,if not then the code will create a new cart item
+      if (itemIdCartItem.includes(itemId) && sizeIdCartItem.includes(sizeId)) {
+        const cartItem = await CartItem.findOne({
+          where: {
+            cartId: userCart.id,
+            itemId,
+          },
+        });
 
-				const cart = await Cart.findOne({
-					where: {
-						id: cartItem.cartId,
-					},
-				});
+        const priceDifference = price * cartItem.quantity - totalPrice;
 
-				const putCartItem = await CartItem.update(
-					{
-						quantity,
-						updatedAt: autoFillUpdatedAt,
-					},
-					{
-						where: {
-							cartId: cartItem.cartId,
-						},
-					}
-				);
+        const putCart = await Cart.update(
+          {
+            totalPrice: userCart.totalPrice - priceDifference,
+            updatedAt: autoFillUpdatedAt,
+          },
+          {
+            where: {
+              id: cartItem.cartId,
+            },
+          }
+        );
 
-				res.status(201).json({
-					cart,
-					cartItem,
-					message: "Quantity updated successfully",
-				});
-			} else {
-				// this is when user try to add another item to the cart
-				const putCart = await Cart.update(
-					{
-						totalPrice: userCart.totalPrice + totalPrice,
-						updatedAt: autoFillUpdatedAt,
-					},
-					{
-						where: {
-							id: userCart.id,
-						},
-					}
-				);
+        const cart = await Cart.findOne({
+          where: {
+            id: cartItem.cartId,
+          },
+        });
 
-				const showCart = await Cart.findOne({
-					where: {
-						id: userCart.id,
-					},
-				});
+        const putCartItem = await CartItem.update(
+          {
+            quantity,
+            updatedAt: autoFillUpdatedAt,
+          },
+          {
+            where: {
+              cartId: cartItem.cartId,
+            },
+          }
+        );
 
-				const newCartItem = await CartItem.create({
-					cartId: userCart.id,
-					itemId,
-					quantity,
-					sizeId,
-					createdAt: autoFillCreateddAt,
-					updatedAt: autoFillUpdatedAt,
-				});
+        res.status(201).json({
+          cart,
+          cartItem,
+          message: "Quantity updated successfully",
+        });
+      } else {
+        // this is when user try to add another item to the cart
+        const putCart = await Cart.update(
+          {
+            totalPrice: userCart.totalPrice + totalPrice,
+            updatedAt: autoFillUpdatedAt,
+          },
+          {
+            where: {
+              id: userCart.id,
+            },
+          }
+        );
 
-				res.status(201).json({
-					showCart,
-					newCartItem,
-					message: "Item added to cart successfully",
-				});
-			}
-		} catch (err) {
-			next(err);
-		}
-	};
+        const showCart = await Cart.findOne({
+          where: {
+            id: userCart.id,
+          },
+        });
 
-	static updateCart = async (req, res, next) => {
-		try {
-			const autoFillUpdatedAt = new Date();
-			const { quantity, sizeId } = req.body;
-			const { cartItemId } = req.params;
-			const userId = req.user.id;
+        const newCartItem = await CartItem.create({
+          cartId: userCart.id,
+          itemId,
+          quantity,
+          sizeId,
+          createdAt: autoFillCreateddAt,
+          updatedAt: autoFillUpdatedAt,
+        });
 
-			// Find the cart to be updated
-			const cart = await Cart.findOne({
-				where: {
-					userId,
-					visibility: "True",
-				},
-			});
-			if (!cart) {
-				throw { name: "ErrorNotFound" };
-				// throw { name: "CartNotFound" };
-			}
+        res.status(201).json({
+          showCart,
+          newCartItem,
+          message: "Item added to cart successfully",
+        });
+      }
+    } catch (err) {
+      next(err);
+    }
+  };
 
-			// find cart item to be updated
-			const cartItem = await CartItem.findOne({
-				where: {
-					id: cartItemId,
-				},
-			});
-			if (!cartItem) {
-				throw { name: "CartNotFound" };
-			}
+  static updateCart = async (req, res, next) => {
+    try {
+      const autoFillUpdatedAt = new Date();
+      const { quantity, sizeId } = req.body;
+      const { cartItemId } = req.params;
+      const userId = req.user.id;
 
-			// check if the item has the size
-			const cekSize = await ItemSize.findOne({
-				where: {
-					itemId: cartItem.itemId,
-					sizeId,
-				},
-			});
-			if (!cekSize) {
-				throw { name: "SizeNotFound" };
-			}
+      // Find the cart to be updated
+      const cart = await Cart.findOne({
+        where: {
+          userId,
+          visibility: "True",
+        },
+      });
+      if (!cart) {
+        throw { name: "CartNotFound" };
+      }
 
-			// find the item
-			const item = await Item.findOne({
-				where: {
-					id: cartItem.itemId,
-				},
-			});
-			if (!item) {
-				throw { name: "ItemNotFound" };
-			}
-			const price = item.price;
+      // find cart item to be updated
+      const cartItem = await CartItem.findOne({
+        where: {
+          id: cartItemId,
+        },
+      });
+      if (!cartItem) {
+        throw { name: "CartNotFound" };
+      }
 
-			const totalPrice = price * quantity;
-			const priceDifference = price * cartItem.quantity - totalPrice;
+      // check if the item has the size
+      const cekSize = await ItemSize.findOne({
+        where: {
+          itemId: cartItem.itemId,
+          sizeId,
+        },
+      });
+      if (!cekSize) {
+        throw { name: "SizeNotFound" };
+      }
 
-			// Update the cart's properties
-			cart.totalPrice -= priceDifference;
-			cart.updatedAt = autoFillUpdatedAt;
-			cartItem.quantity = quantity;
-			cartItem.sizeId = sizeId;
-			cartItem.updatedAt = autoFillUpdatedAt;
+      // find the item
+      const item = await Item.findOne({
+        where: {
+          id: cartItem.itemId,
+        },
+      });
+      if (!item) {
+        throw { name: "ItemNotFound" };
+      }
+      const price = item.price;
 
-			// Save the updated item
-			await cart.save();
-			await cartItem.save();
+      const totalPrice = price * quantity;
+      const priceDifference = price * cartItem.quantity - totalPrice;
 
-			res.status(200).json({
-				cart,
-				cartItem,
-				message: "Cart has been updated",
-			});
-		} catch (err) {
-			next(err);
-		}
-	};
+      // Update the cart's properties
+      cart.totalPrice -= priceDifference;
+      cart.updatedAt = autoFillUpdatedAt;
+      cartItem.quantity = quantity;
+      cartItem.sizeId = sizeId;
+      cartItem.updatedAt = autoFillUpdatedAt;
 
-	static deleteCart = async (req, res, next) => {
-		try {
-			const userId = req.user.id;
+      // Save the updated item
+      await cart.save();
+      await cartItem.save();
 
-			// check if the user has active cart
-			const cart = await Cart.findOne({
-				where: {
-					userId,
-					visibility: "True",
-				},
-			});
-			if (!cart) {
-				throw { name: "CartNotFound" };
-			}
+      res.status(200).json({
+        cart,
+        cartItem,
+        message: "Cart has been updated",
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
 
-			const softDelete = await Cart.destroy({
-				where: {
-					id: cart.id,
-				},
-				// enable below code if you really want to delete the data(hard delete)
-				// force: true,
-			});
-			cart.visibility = "False";
-			await cart.save();
+  static deleteCart = async (req, res, next) => {
+    try {
+      const userId = req.user.id;
 
-			const deleteCartItem = await CartItem.destroy({
-				where: {
-					cartId: cart.id,
-				},
-			});
+      // check if the user has active cart
+      const cart = await Cart.findOne({
+        where: {
+          userId,
+          visibility: "True",
+        },
+      });
+      if (!cart) {
+        throw { name: "CartNotFound" };
+      }
 
-			res.status(201).json({ message: "Delete success" });
-		} catch (err) {
-			next(err);
-		}
-	};
+      const softDelete = await Cart.destroy({
+        where: {
+          id: cart.id,
+        },
+        // enable below code if you really want to delete the data(hard delete)
+        // force: true,
+      });
+      cart.visibility = "False";
+      await cart.save();
 
-	static deleteItemCart = async (req, res, next) => {
-		try {
-			const autoFillUpdatedAt = new Date();
-			const { cartItemId } = req.params;
-			const userId = req.user.id;
+      const deleteCartItem = await CartItem.destroy({
+        where: {
+          cartId: cart.id,
+        },
+      });
 
-			const cartItem = await CartItem.findOne({
-				where: {
-					id: cartItemId,
-				},
-			});
-			if (!cartItem) {
-				throw { name: "CartNotFound" };
-			}
+      res.status(201).json({ message: "Delete success" });
+    } catch (err) {
+      next(err);
+    }
+  };
 
-			const item = await Item.findOne({
-				where: {
-					id: cartItem.itemId,
-				},
-			});
+  static deleteItemCart = async (req, res, next) => {
+    try {
+      const autoFillUpdatedAt = new Date();
+      const { cartItemId } = req.params;
+      const userId = req.user.id;
 
-			const findCart = await Cart.findOne({
-				where: {
-					userId,
-					visibility: "True",
-				},
-			});
-			if (!find) {
-				throw { name: "ErrorNotFound" };
-				// throw { name: "CartNotFound" };
-			}
+      const cartItem = await CartItem.findOne({
+        where: {
+          id: cartItemId,
+        },
+      });
+      if (!cartItem) {
+        throw { name: "CartNotFound" };
+      }
 
-			const currentTotalPrice = findCart.totalPrice;
-			const minus = item.price * cartItem.quantity;
+      const item = await Item.findOne({
+        where: {
+          id: cartItem.itemId,
+        },
+      });
 
-			const cart = await Cart.update(
-				{
-					totalPrice: currentTotalPrice - minus,
-					updatedAt: autoFillUpdatedAt,
-				},
-				{
-					where: {
-						id: findCart.id,
-					},
-				}
-			);
+      const findCart = await Cart.findOne({
+        where: {
+          userId,
+          visibility: "True",
+        },
+      });
+      if (!findCart) {
+        throw { name: "CartNotFound" };
+      }
 
-			const deleteCartItem = await CartItem.destroy({
-				where: {
-					id: cartItemId,
-				},
-			});
+      const currentTotalPrice = findCart.totalPrice;
+      const minus = item.price * cartItem.quantity;
 
-			res.status(200).json({
-				message: "Cart Item has been deleted",
-			});
-		} catch (err) {
-			next(err);
-		}
-	};
+      const cart = await Cart.update(
+        {
+          totalPrice: currentTotalPrice - minus,
+          updatedAt: autoFillUpdatedAt,
+        },
+        {
+          where: {
+            id: findCart.id,
+          },
+        }
+      );
 
-	static addToCart = async (req, res, next) => {
-		try {
-			const userId = req.user.id;
-			const { itemId, sizeId, quantity } = req.body;
-			
-			//find Address
-			const address = await Address.findByPk(userId,{
-				where: {
-					userId: userId
-				}
-			});
+      const deleteCartItem = await CartItem.destroy({
+        where: {
+          id: cartItemId,
+        },
+      });
 
-			//find Item
-			const itemSelect = await Item.findOne({
-				where: {
-					id: itemId,
-				},
-			});
-			if (!itemSelect) {
-				throw { name: "ItemNotFound" };
-			}
-			//find Size
-			const sizeSelect = await Size.findOne({
-				where: {
-					id: sizeId,
-				},
-			});
-			
-			//total Price
-			const totalPrice = itemSelect.price * quantity;
-
-			const cart = await Cart.create({
-			  userId: userId,
-			  addressId:address.id,
-			  totalPrice: totalPrice,
-			  visibility:"True",
-			});
-
-			const cartItem = await CartItem.create({
-				cartId: cart.id,
-				itemId: itemSelect.id,
-				sizeId: sizeSelect.id,
-				quantity: quantity
-			});
-			
-			// await cart.save();
-			return res.status(201).json({ cart,cartItem });
-		} catch (err) {
-			next(err);
-		}
-	}
-
-	static showCarts = async (req, res, next) => {
-		try {
-			const userId = req.user.id;
-			const carts = await Cart.findAll({
-				where: {
-					userId: userId
-				},
-				include: [
-					{
-						model: Item,
-						as: "cartItem",
-						through: { attributes: ["quantity"] },
-						attributes: ["itemName", "price", "imageUrl"],
-						include: [
-							{
-								model: CartItem,
-								attributes: [ "sizeId" ],
-								include: [
-									{
-										model: Size,
-										attributes: ["size"]
-									}
-								]
-							}
-						]
-					},
-				],
-			})
-			res.status(200).json(carts);
-		} catch (err) {
-			next(err);
-		}
-	}
-	
+      res.status(200).json({
+        message: "Cart Item has been deleted",
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
 }
 
 module.exports = CartController;
--- routes/cart.js
+++ routes/cart.js
@@ -3,15 +3,11 @@ const router = express.Router();
 const { Auth } = require("../middlewares/auth");
 const CartController = require("../controllers/cartController.js");
 
-//middleware
-router.use( Auth.author);
-
+router.use(Auth.author);
 router.get("/get/", CartController.showAllCartByUserId);
 router.post("/add/", CartController.addCart);
 router.put("/update/:cartItemId", CartController.updateCart);
 router.delete("/delete/", CartController.deleteCart);
 router.delete("/deleteCartItem/:cartItemId", CartController.deleteItemCart);
 
-router.post("/addcart", CartController.addToCart);
-router.get("/", CartController.showCarts);
 module.exports = router;
