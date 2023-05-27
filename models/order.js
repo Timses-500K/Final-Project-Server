f2b1d4a4fae60bae59a7502adcd214d904e464c4
WilliamFirman <william.firman25@gmail.com>
2023-05-28 05:16:48 +0700

    Added table addressId

--- models/order.js
+++ models/order.js
@@ -1,23 +1,29 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Order extends Model {
-
     static associate(models) {
-      Order.belongsTo(models.User,{foreignKey: "userId"});
-      Order.belongsToMany(models.Item,{foreignKey: "orderId", through: models.OrderItem,as:"orderItem"})
+      Order.belongsTo(models.User, { foreignKey: "userId" });
+      Order.belongsToMany(models.Item, { foreignKey: "orderId", through: models.OrderItem, as: "orderItem" });
+      Order.belongsTo(models.Cart, { foreignKey: "cartId" });
+      Order.hasMany(models.OrderItem, { foreignKey: "orderId" });
+      Order.belongsTo(models.Address, { foreignKey: "addressId" });
     }
   }
-  Order.init({
-    userId: DataTypes.INTEGER,
-    subtotal: DataTypes.FLOAT,
-    totalPrice: DataTypes.FLOAT,
-    status: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Order',
-  });
+  Order.init(
+    {
+      userId: DataTypes.INTEGER,
+      cartId: DataTypes.INTEGER,
+      addressId: DataTypes.INTEGER,
+      subtotal: DataTypes.FLOAT,
+      totalPrice: DataTypes.FLOAT,
+      status: DataTypes.STRING,
+      payment: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Order",
+    }
+  );
   return Order;
 };
